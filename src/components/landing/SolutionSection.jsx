4900abad4728e5fa8691578f6ed4020136d5ec91
josemanitasrobles-ops <josemanitasrobles@gmail.com>
2026-09-12 14:30:06 +0200

    Update SolutionSection.jsx

--- src/components/landing/SolutionSection.jsx
+++ src/components/landing/SolutionSection.jsx
@@ -4,10 +4,10 @@ import { Scissors, Sparkles, ShieldCheck } from "lucide-react";
 import WhatsAppButton from "./WhatsAppButton";
 
 // Importación de tus imágenes locales optimizadas
-import fondoImg from "../assets/fondo.webp";
-import huertoImg from "../assets/huerto.webp";
-import jardinImg from "../assets/jardin.webp";
-import palaImg from "../assets/pala.webp";
+import fondoImg from "../assets/FONDO.webp";
+import huertoImg from "../assets/HUERTO.webp";
+import jardinImg from "../assets/JARDIN.webp";
+import palaImg from "../assets/PALA.webp";
 
 const steps = [
   {
